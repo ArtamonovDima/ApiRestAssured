58f9f1728b35cb04d80384ef16367957ba1c20d4
Artamonov Dima <dima.artamonov1989@gmail.com>
2018-07-02 14:35:38 +0300

    +allure

--- src/test/java/RestTest.java
+++ src/test/java/RestTest.java
@@ -1,23 +1,25 @@
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.basePath;
-import static io.restassured.RestAssured.baseURI;
-import static io.restassured.RestAssured.given;
+import static com.sun.scenario.Settings.get;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
+
 
 public class RestTest extends BaseRestTest {
 
         @Test (description = "Проверка, что существует персонаж “Luke Skywalker” c home planet Tatooine")
          public void LukeSkywalkerIsAlive(){
 
-          given().contentType(ContentType.JSON)
+            given().contentType(ContentType.JSON)
                .when().get(baseURI+basePath+"/people/1")
                .then().body("name", equalTo("Luke Skywalker"));
 
-          given().contentType(ContentType.JSON)
+            given().contentType(ContentType.JSON)
                   .when().get(baseURI+basePath+"/planets/1")
                   .then().body("name", equalTo("Tatooine"));
+
+
           }
 
         @Test (description = "Проверка, что количество персонажей равно 87")
